7aa25320ab449fdd030267e3b810024efebaddce
jdvega <jdvega@idealista.com>
2017-09-04 15:59:00 +0200

    green: parametrized test with same samples for encrypt/decrypt

---
+++ src/test/java/com/idealista/fpe/ff1/FF1AlgorithmWithRadix10NoEmptyDataShould.java
@@ -0,0 +1,65 @@
+package com.idealista.fpe.ff1;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.crypto.Cipher;
+import javax.crypto.NoSuchPaddingException;
+import javax.naming.ldap.HasControls;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@RunWith(Parameterized.class)
+public class FF1AlgorithmWithRadix10NoEmptyDataShould {
+
+    public FF1AlgorithmWithRadix10NoEmptyDataShould() throws NoSuchPaddingException, NoSuchAlgorithmException {
+    }
+
+    @Parameterized.Parameters(name = "{index}: plain text is {0}")
+    public static Iterable<int[]> data() {
+        return Arrays.asList(
+                new int[] {1, 2, 3, 4, 5, 6, 7, 8, 0, 9, 4, 3, 2},
+                new int[] {4, 4, 5, 4, 7, 0, 9, 4, 3, 2},
+                new int[] {7, 5, 6}
+        );
+    }
+
+    @Parameterized.Parameter
+    public int[] input;
+
+    Integer radix = 10;
+    byte[] key = { (byte) 0x2B, (byte) 0x7E, (byte) 0x15, (byte) 0x16, (byte) 0x28, (byte) 0xAE, (byte) 0xD2,
+            (byte) 0xA6, (byte) 0xAB, (byte) 0xF7, (byte) 0x15, (byte) 0x88, (byte) 0x09, (byte) 0xCF, (byte) 0x4F,
+            (byte) 0x3C };
+
+    byte[] tweak = {
+            (byte) 0x39, (byte) 0x38, (byte) 0x37, (byte) 0x36, (byte) 0x35, (byte) 0x34, (byte) 0x33, (byte) 0x32, (byte) 0x31, (byte) 0x30
+    };
+
+
+    Cipher cipher = Cipher.getInstance("AES/CBC/NoPadding");
+
+    @Test
+    public void given_a_plain_text_return_the_cipher_text () throws Exception {
+        int[] cipherText = FF1Algorithm.encrypt(input, radix, key, tweak, cipher);
+        assertThat(input.length, is(cipherText.length));
+        assertThat(input, is(FF1Algorithm.decrypt(cipherText, radix, key, tweak, cipher)));
+
+    }
+
+    @Test
+    public void given_a_cipher_text_return_the_plain_text () throws Exception {
+        int[] plainText = FF1Algorithm.decrypt(input, radix, key, tweak, cipher);
+        assertThat(input.length, is(plainText.length));
+        assertThat(input, is(FF1Algorithm.encrypt(plainText, radix, key, tweak, cipher)));
+
+    }
+
+}
