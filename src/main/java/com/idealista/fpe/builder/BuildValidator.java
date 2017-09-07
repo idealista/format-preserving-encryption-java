f79c2af8b6684efbf0113b12aca6641ad29fd67d
jdvega <jdvega@idealista.com>
2017-09-07 12:03:51 +0200

    refactor: use instance methods

--- src/main/java/com/idealista/fpe/builder/BuildValidator.java
+++ src/main/java/com/idealista/fpe/builder/BuildValidator.java
@@ -4,22 +4,34 @@ import java.util.Map;
 
 public class BuildValidator {
 
-    public static void radix(int radix) {
+    private final int radix;
+    private final int minLength;
+    private final int maxLength;
+    private final byte[] tweak;
+
+    public BuildValidator(int radix, int minLength, int maxLength, byte[] tweak) {
+        this.radix = radix;
+        this.minLength = minLength;
+        this.maxLength = maxLength;
+        this.tweak = tweak;
+    }
+
+    public void radix() {
         if (radix < 2 || radix > Math.pow(2d, 16d))
             throw new IllegalArgumentException("radix not in: [ 2 .. 2 ^ 16 ]");
     }
 
-    public static void textSize(int minLength, int maxLength) {
+    public void textSize() {
         if (minLength < 2  || minLength >= maxLength)
             throw new IllegalArgumentException("minLength not in: [ 2 .." + maxLength + " )");
     }
 
-    public static void radixToMinLength(int radix, int minLength) {
+    public void radixToMinLength() {
         if (Math.pow(radix, minLength) > 100)
             throw new IllegalArgumentException("radix ^ minLength is greater than 100");
     }
 
-    public static void tweakSize(byte[] tweak, int maxLength) {
+    public void tweakSize() {
         if (tweak.length > maxLength)
             throw new IllegalArgumentException("tweak length should be less than maxLength");
     }
--- src/test/java/com/idealista/fpe/builder/BuilValidatorShould.java
+++ src/test/java/com/idealista/fpe/builder/BuilValidatorShould.java
@@ -5,44 +5,45 @@ import org.junit.Test;
 public class BuilValidatorShould {
     /*
      - null protection for all inputs
-     -  tweak should be lower that maxlen
      -  n ∈ [minlen .. maxlen];
      -  0 < plainText[i] <= radix
      */
 
+
+
     @Test (expected = IllegalArgumentException.class)
     public void throws_an_exception_when_radix_is_less_than_two () {
-        BuildValidator.radix(1);
+        new BuildValidator(1, 0, 0, new byte[0]).radix();
     }
 
     @Test (expected = IllegalArgumentException.class)
     public void throws_an_exception_when_radix_is_greater_than_two_to_sixteen () {
-        BuildValidator.radix((int) (Math.pow(2d, 16d) * 2));
+        new BuildValidator((int) (Math.pow(2d, 16d) * 2), 0, 0, new byte[0]).radix();
     }
 
     @Test (expected = IllegalArgumentException.class)
     public void throws_an_exception_when_radix_to_minLength_is_greater_than_one_hundred () {
-        BuildValidator.radixToMinLength(11, 2);
+        new BuildValidator(11, 2, 0, new byte[0]).radixToMinLength();
     }
 
     @Test (expected = IllegalArgumentException.class)
     public void throws_an_exception_when_min_length_is_less_than_two () {
         int anyNumber = 1234;
-        BuildValidator.textSize(1, anyNumber);
+        new BuildValidator(1, 1, anyNumber, new byte[0]).textSize();
     }
 
     @Test (expected = IllegalArgumentException.class)
     public void throws_an_exception_when_min_length_is_greater_or_equal_than_maxLength () {
         int anyLoweNumber = 1234;
         int minLength = anyLoweNumber + 10;
-        BuildValidator.textSize(minLength, anyLoweNumber);
+        new BuildValidator(1, minLength, anyLoweNumber, new byte[0]).textSize();
     }
 
     @Test (expected = IllegalArgumentException.class)
     public void throws_an_exception_when_tweak_length_is_greater_than_maxLength () {
         int aMaxLength = 10;
         byte[] anyLargeTweak = new byte[aMaxLength * 2];
-        BuildValidator.tweakSize(anyLargeTweak, aMaxLength);
+        new BuildValidator(1, 0, aMaxLength, anyLargeTweak).tweakSize();
     }
 
 }
