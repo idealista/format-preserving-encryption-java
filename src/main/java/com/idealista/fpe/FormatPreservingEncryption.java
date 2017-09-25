84fa1180193373ae57eb4d06e56f8bfe8811823c
jdvega <jdvega@idealista.com>
2017-09-25 16:49:14 +0200

    red: with_default_parameters_given_a_valid_domain_text_and_an_empty_tweak_should_return_a_valid_cipher_text

--- src/main/java/com/idealista/fpe/FormatPreservingEncryption.java
+++ src/main/java/com/idealista/fpe/FormatPreservingEncryption.java
@@ -1,4 +1,28 @@
 package com.idealista.fpe;
 
+import com.idealista.fpe.algorithm.Cipherer;
+import com.idealista.fpe.component.functions.prf.PseudorandomFunction;
+import com.idealista.fpe.config.Domain;
+import com.idealista.fpe.config.LengthRange;
+
 public class FormatPreservingEncryption {
+    private final Cipherer cipherer;
+    private final Domain selectedDomain;
+    private final PseudorandomFunction selectedPRF;
+    private final LengthRange lengthRange;
+
+    public FormatPreservingEncryption(Cipherer cipherer, Domain selectedDomain, PseudorandomFunction selectedPRF, LengthRange lengthRange) {
+        this.cipherer = cipherer;
+        this.selectedDomain = selectedDomain;
+        this.selectedPRF = selectedPRF;
+        this.lengthRange = lengthRange;
+    }
+
+    public String encrypt(String plainTest, byte[] tweak) {
+        return new String();
+    }
+
+    public String decrypt(String cipherText, byte[] tweak) {
+        return new String();
+    }
 }
---
+++ src/test/java/com/idealista/fpe/FormatPreservingEncryptionShould.java
@@ -0,0 +1,37 @@
+package com.idealista.fpe;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+import com.idealista.fpe.builder.FormatPreservingEncryptionBuilder;
+import com.idealista.fpe.config.basic.BasicAlphabet;
+
+public class FormatPreservingEncryptionShould {
+
+    private byte[] anyKey = new byte[]{
+            (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00,
+            (byte) 0x01, (byte) 0x01, (byte) 0x01, (byte) 0x01,
+            (byte) 0x02, (byte) 0x02, (byte) 0x02, (byte) 0x02,
+            (byte) 0x03, (byte) 0x03, (byte) 0x03, (byte) 0x03
+    };
+
+    @Test
+    public void with_default_parameters_given_a_valid_domain_text_and_an_empty_tweak_should_return_a_valid_cipher_text () {
+        FormatPreservingEncryption formatPreservingEncryption = FormatPreservingEncryptionBuilder.ff1Implementation()
+                .withDefaultDomain()
+                .withDefaultPseudoRandomFunction(anyKey)
+                .withDefaultLengthRange()
+                .build();
+
+        String anyPlainTextOfDefaultDomain = "abcdefgsffsdfe";
+
+        String cipherText = formatPreservingEncryption.encrypt(anyPlainTextOfDefaultDomain, new byte[0]);
+
+        assertThat(cipherText).hasSameSizeAs(anyPlainTextOfDefaultDomain);
+        assertThat(new BasicAlphabet().availableCharacters()).contains(cipherText.toCharArray());
+        String reverse = formatPreservingEncryption.decrypt(cipherText, new byte[0]);
+        assertThat(reverse).isEqualTo(anyPlainTextOfDefaultDomain);
+    }
+
+}
