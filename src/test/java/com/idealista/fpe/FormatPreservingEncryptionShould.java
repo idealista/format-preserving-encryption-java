a12854fa905f51845b6e72d9db240be3329f588d
jdvega <jdvega@idealista.com>
2017-09-25 17:58:47 +0200

    green: with_default_parameters_given_a_valid_domain_text_and_two_different_tweaks_should_return_a_invalid_cipher_text

--- src/test/java/com/idealista/fpe/FormatPreservingEncryptionShould.java
+++ src/test/java/com/idealista/fpe/FormatPreservingEncryptionShould.java
@@ -18,11 +18,7 @@ public class FormatPreservingEncryptionShould {
 
     @Test
     public void with_default_parameters_given_a_valid_domain_text_and_an_empty_tweak_should_return_a_valid_cipher_text () {
-        FormatPreservingEncryption formatPreservingEncryption = FormatPreservingEncryptionBuilder.ff1Implementation()
-                .withDefaultDomain()
-                .withDefaultPseudoRandomFunction(anyKey)
-                .withDefaultLengthRange()
-                .build();
+        FormatPreservingEncryption formatPreservingEncryption = defaultFormatPreservingEncryption();
 
         String anyPlainTextOfDefaultDomain = "abcdefgsffsdfe";
 
@@ -36,11 +32,7 @@ public class FormatPreservingEncryptionShould {
 
     @Test
     public void with_default_parameters_given_a_valid_domain_text_and_a_valid_tweak_should_return_a_valid_cipher_text () {
-        FormatPreservingEncryption formatPreservingEncryption = FormatPreservingEncryptionBuilder.ff1Implementation()
-                .withDefaultDomain()
-                .withDefaultPseudoRandomFunction(anyKey)
-                .withDefaultLengthRange()
-                .build();
+        FormatPreservingEncryption formatPreservingEncryption = defaultFormatPreservingEncryption();
 
         String anyPlainTextOfDefaultDomain = "abcdefgsffsdfe";
         byte[] anyTweak = new byte[]{
@@ -53,6 +45,35 @@ public class FormatPreservingEncryptionShould {
         assertThat(new BasicAlphabet().availableCharacters()).contains(cipherText.toCharArray());
         String reverse = formatPreservingEncryption.decrypt(cipherText, anyTweak);
         assertThat(reverse).isEqualTo(anyPlainTextOfDefaultDomain);
+    }
+
+    @Test
+    public void with_default_parameters_given_a_valid_domain_text_and_two_different_tweaks_should_return_a_invalid_cipher_text() {
+        FormatPreservingEncryption formatPreservingEncryption = defaultFormatPreservingEncryption();
+
+        String anyPlainTextOfDefaultDomain = "abcdefgsffsdfe";
+        byte[] oneTweak = new byte[]{
+                (byte) 0x01, (byte) 0x03, (byte) 0x02, (byte) 0x04
+        };
+
+        byte[] otherTweak = new byte[]{
+                (byte) 0x01, (byte) 0x02, (byte) 0x03, (byte) 0x04
+        };
+
+        String cipherText = formatPreservingEncryption.encrypt(anyPlainTextOfDefaultDomain, oneTweak);
+
+        assertThat(cipherText).hasSameSizeAs(anyPlainTextOfDefaultDomain);
+        assertThat(new BasicAlphabet().availableCharacters()).contains(cipherText.toCharArray());
+        String reverse = formatPreservingEncryption.decrypt(cipherText, otherTweak);
+        assertThat(reverse).isNotEqualTo(anyPlainTextOfDefaultDomain);
+    }
+
+    private FormatPreservingEncryption defaultFormatPreservingEncryption() {
+        return FormatPreservingEncryptionBuilder.ff1Implementation()
+                    .withDefaultDomain()
+                    .withDefaultPseudoRandomFunction(anyKey)
+                    .withDefaultLengthRange()
+                    .build();
     }
 
 }
