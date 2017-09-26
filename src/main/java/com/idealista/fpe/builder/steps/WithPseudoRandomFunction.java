f6c537cbe5dcf79329f492834b905b7ce3b1add7
jdvega <jdvega@idealista.com>
2017-09-26 13:31:42 +0200

    refactor: rename param

--- src/main/java/com/idealista/fpe/builder/steps/WithPseudoRandomFunction.java
+++ src/main/java/com/idealista/fpe/builder/steps/WithPseudoRandomFunction.java
@@ -5,5 +5,5 @@ import com.idealista.fpe.component.functions.prf.PseudoRandomFunction;
 public interface WithPseudoRandomFunction {
     WithLengthRange withPseudoRandomFunction(PseudoRandomFunction pseudoRandomFunction);
 
-    WithLengthRange withDefaultPseudoRandomFunction(byte[] anyKey);
+    WithLengthRange withDefaultPseudoRandomFunction(byte[] key);
 }
