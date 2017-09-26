135d8148e5689b0969f70da4a48723502ce60b27
jdvega <jdvega@idealista.com>
2017-09-26 13:36:40 +0200

    refactor: remove redundant public

--- src/main/java/com/idealista/fpe/config/Domain.java
+++ src/main/java/com/idealista/fpe/config/Domain.java
@@ -1,7 +1,7 @@
 package com.idealista.fpe.config;
 
 public interface Domain {
-    public Alphabet alphabet();
-    public int[] transform(String data);
-    public String transform(int[] data);
+    Alphabet alphabet();
+    int[] transform(String data);
+    String transform(int[] data);
 }
