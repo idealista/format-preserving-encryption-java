a3b30a0745331456f50a2c977da657496cc4bc55
jdvega <jdvega@idealista.com>
2017-09-26 13:43:20 +0200

    refactor: make alphabet characters static

--- src/main/java/com/idealista/fpe/config/basic/BasicAlphabet.java
+++ src/main/java/com/idealista/fpe/config/basic/BasicAlphabet.java
@@ -4,16 +4,16 @@ import com.idealista.fpe.config.Alphabet;
 
 public class BasicAlphabet implements Alphabet{
 
-    private final char[] availableCharacters = new char[] {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+    private static final char[] LOWER_CASE_CHARS = new char[] {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
             'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
     @Override
     public char[] availableCharacters() {
-        return availableCharacters;
+        return LOWER_CASE_CHARS;
     }
 
     @Override
     public Integer radix() {
-        return availableCharacters.length;
+        return LOWER_CASE_CHARS.length;
     }
 }
