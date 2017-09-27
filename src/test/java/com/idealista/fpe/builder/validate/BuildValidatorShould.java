ef4e39f41a0a8a70b871d9194f2c72ae8e3743d1
jdvega <jdvega@idealista.com>
2017-09-27 10:14:55 +0200

    rename: throws -> throw

--- src/test/java/com/idealista/fpe/builder/validate/BuildValidatorShould.java
+++ src/test/java/com/idealista/fpe/builder/validate/BuildValidatorShould.java
@@ -6,28 +6,28 @@ public class BuildValidatorShould {
 
 
     @Test (expected = IllegalArgumentException.class)
-    public void throws_an_exception_when_radix_is_less_than_two () {
+    public void throw_an_exception_when_radix_is_less_than_two () {
         new BuildValidator(1, 0, 0).radix();
     }
 
     @Test (expected = IllegalArgumentException.class)
-    public void throws_an_exception_when_radix_is_greater_than_two_to_sixteen () {
+    public void throw_an_exception_when_radix_is_greater_than_two_to_sixteen () {
         new BuildValidator((int) (Math.pow(2d, 16d) * 2), 0, 0).radix();
     }
 
     @Test (expected = IllegalArgumentException.class)
-    public void throws_an_exception_when_radix_to_minLength_is_greater_than_one_hundred () {
+    public void throw_an_exception_when_radix_to_minLength_is_greater_than_one_hundred () {
         new BuildValidator(8, 2, 0).radixToMinLength();
     }
 
     @Test (expected = IllegalArgumentException.class)
-    public void throws_an_exception_when_min_length_is_less_than_two () {
+    public void throw_an_exception_when_min_length_is_less_than_two () {
         int anyNumber = 1234;
         new BuildValidator(1, 1, anyNumber).textSizeLimits();
     }
 
     @Test (expected = IllegalArgumentException.class)
-    public void throws_an_exception_when_min_length_is_greater_or_equal_than_maxLength () {
+    public void throw_an_exception_when_min_length_is_greater_or_equal_than_maxLength () {
         int anyLoweNumber = 1234;
         int minLength = anyLoweNumber + 10;
         new BuildValidator(1, minLength, anyLoweNumber).textSizeLimits();
