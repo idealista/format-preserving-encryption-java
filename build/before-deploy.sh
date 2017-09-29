3984242828ecc6e40be29fe6b3a73e64eaf8d671
jdvega <jdvega@idealista.com>
2017-09-29 13:22:48 +0200

    update travis ossrh credentials

--- build/before-deploy.sh
+++ build/before-deploy.sh
@@ -2,5 +2,5 @@
 
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     openssl aes-256-cbc -K $encrypted_d39b6f1d3f80_key -iv $encrypted_d39b6f1d3f80_iv -in build/codesigning.asc.enc -out build/codesigning.asc -d
-    gpg --fast-import build/signingkey.asc
+    gpg --fast-import build/codesigning.asc
 fi
